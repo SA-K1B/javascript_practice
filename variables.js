00eeb6287e1d2c983aca3639b3c7371958afe2ef
SA-K1B <tareqshakib789@gmail.com>
2025-02-01 19:56:07 +0600

    add scope of variables

--- variables.js
+++ variables.js
@@ -1,9 +1,30 @@
 //console.log(name)  // undefined
 //console.log(id)   // reference error
+//console.log(level) // same error as let
+
 var name = "john"
 let id = 1
 const level = 2
+
 //level = 3   // not possible..cannot be changed
 //level = level+1
-console.log("var")
-console.log(level)
+
+// console.log("var")
+// console.log(level)
+
+/*
+if(id == 1){
+    var first = "john"
+}
+console.log(first)
+*/    
+//this works because scope of var is global, meaning it is accessible from start to end of the function where it is declared
+
+
+/*
+if(id == 1){
+    let first = "john"
+}
+console.log(first)
+*/
+// does not work..let is alive only inside the block {}
