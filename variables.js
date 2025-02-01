10bf7f59b504fe7dc20d587c0961d6e072b87165
SA-K1B <tareqshakib789@gmail.com>
2025-02-01 19:35:28 +0600

    add variable.js

---
+++ variables.js
@@ -0,0 +1,9 @@
+//console.log(name)  // undefined
+//console.log(id)   // reference error
+var name = "john"
+let id = 1
+const level = 2
+//level = 3   // not possible..cannot be changed
+//level = level+1
+console.log("var")
+console.log(level)
